f1b5b9c41821c1df71d70c78c0de64fdf9dd12da
jalilbengoufa <jalilbengoufa@gmail.com>
2022-12-01 00:42:49 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using adventofcode_2022.Day1;
+
+Console.WriteLine("Hello, World!");
+
+
+Day1 objExample = new Day1();
+objExample.solve();
+
+
